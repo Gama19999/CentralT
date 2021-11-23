8cbf3f09f94fa3f4ce5c158d43f624582a03feff
ChrisSO20 <sa366090@uaeh.edu.mx>
2021-11-23 15:20:53 -0600

    Corrección de Ortografía

--- src/main/java/centralt/centralt/Main.java
+++ src/main/java/centralt/centralt/Main.java
@@ -16,11 +16,14 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240); // 320, 240
+
         stage.setTitle("Central Telefónica");
         stage.setMaximized(true);
         stage.getIcons().add(new Image("https://lh3.googleusercontent.com/a-/AOh14GgMXo97s6WQkT7wT4XxMgN05-SNgZHfq00A-Vj_pg=s83"));
         stage.setScene(scene);
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
